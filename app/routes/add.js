21f7381d200e6beb873d9355c6e62ae72dbace41
Kate Sowles <katesowles@gmail.com>
2019-03-31 20:20:41 -0700

    'add' route now gets list model data from storageSvc, displays list above what will be 'add' component

--- app/routes/add.js
+++ app/routes/add.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  storageSvc: service('storage'),
+
+  // NOTE: i'm sure there MUST be a better way to do this that doens't involve the ember-view tag to reload with the same data
+  model () {
+    return {
+      list: this.storageSvc.items,
+    }
+  }
 });
