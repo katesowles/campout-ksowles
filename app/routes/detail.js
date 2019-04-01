6bc9a5b1e32341185dddad18262352d4e699b1d2
Kate Sowles <katesowles@gmail.com>
2019-03-31 19:52:18 -0700

    'detail' route updated to get campground object from storageSvc that matches the campgroun_id URL param.

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  storageSvc: service('storage'),
+
+  model (params) {
+    return this.storageSvc.starters.find(x => x.id === params.campground_id);
+  }
 });
