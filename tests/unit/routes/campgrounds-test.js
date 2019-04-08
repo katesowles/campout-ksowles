465e194cc6ad07a2f9196c2a64d03854cff62ecd
Kate Sowles <katesowles@gmail.com>
2019-04-08 03:18:38 -0700

    added mirage test capability to campgrounds unit test file

--- tests/unit/routes/campgrounds-test.js
+++ tests/unit/routes/campgrounds-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage'
 
 module('Unit | Route | campground-list', function(hooks) {
   setupTest(hooks);
+  setupMirage(hooks);
 
   test('should show campgrounds as home page', async function(assert) {
     await visit('/');
